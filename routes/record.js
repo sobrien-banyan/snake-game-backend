7039ca9810ab0048cc9c8662f7ecd46c8ea6a79d
sobrien-banyan <sobrien@banyanlabs.io>
2023-02-16 15:12:17 -0700

    x

--- routes/record.js
+++ routes/record.js
@@ -52,29 +52,11 @@ recordRoutes.route("/record/:id").get(function (req, res) {
 });
  
 // This section will help you create a new record.
-recordRoutes.route("/record/add").post(function (req, res) {
+recordRoutes.route("/record/add").post(function (req, response) {
  let db_connect = dbo.getDb();
- db_connect.collection("name_score").insertOne(req.body);
- db_connect
-   .collection("name_score")
-   .find()
-   .forEach(element => {
-    data.push({
-      id: element._id.toString(),
-      name: element.name,
-      score: element.score,
-    });
-   }).then(() => {
-    const sortedList = data.sort((a,b) => {
-      if (a.score > b.score) {
-        return -1;
-      }
-      if (a.score < b.score) {
-        return 1;
-      }
-      return 0;
-    })
-    res.json(sortedList.slice(0, 20))});
+ db_connect.collection("name_score").insertOne(req.body).then(res => 
+  response.json(res)
+  ).catch(err => console.error(err.message));
 });
  
 // This section will help you update a record by id.
